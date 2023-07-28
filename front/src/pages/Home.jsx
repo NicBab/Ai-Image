dce087aea4bf2a419bcdfdd15f8bc80aee82001c
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2023-07-27 19:35:38 -0500

    save post image to cloudinary and download functions & cardc component working

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -15,6 +15,32 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
 
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -46,7 +72,7 @@ const Home = () => {
               {searchText ? (
                 <RenderCards data={[]} title="No Search Results Found"/>
               ) : (
-                <RenderCards data={[]} title="No Posts Found"/>
+                <RenderCards data={allPosts} title="No Posts Found"/>
               )}
             </div>
           </>
